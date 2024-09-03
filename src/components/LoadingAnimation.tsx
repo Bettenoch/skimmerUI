bcdf4cd48b1a60188395972ce49c3e9d593e3860
Bettenoch <bettenoch254@gmail.com>
2024-09-03 10:59:19 +0300

    Create loading animation

---
+++ src/components/LoadingAnimation.tsx
@@ -0,0 +1,11 @@
+
+
+const LoadingAnimation = () => {
+    return (
+        <div className="ring">Loading
+            <span className="loader"></span>
+        </div>
+    )
+}
+
+export default LoadingAnimation
