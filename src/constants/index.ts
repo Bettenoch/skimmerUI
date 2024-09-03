9b4c8ca3cb029ee8b293588b2a7fde94fdc25e95
Bettenoch <bettenoch254@gmail.com>
2024-09-03 12:42:16 +0300

    Create constants page.

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+export const mainLinks = [
+    {
+        imgUrl: "/icons/home.svg",
+        route: "/",
+        label: "Home"
+    },
+    {
+        imgUrl: "/icons/discover.svg",
+        route: "/about",
+        label: "About"
+    },
+    {
+        imgUrl: "/icons/microphone.svg",
+        route: "/services",
+        label: "services"
+    },
+];
