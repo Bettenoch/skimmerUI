ad299e19d2e75d2b34502bc1c42415003b236fe2
Bettenoch <bettenoch254@gmail.com>
2024-09-05 18:44:43 +0300

    Add components.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import Layout from "@/components/Layout";
 import Hero from "@/sections/home/Hero";
+import Headline from "@/sections/home/Headline";
+import Features from "@/sections/home/Features";
 
 const Home: React.FC = () => {
   return (
     <Layout>
       <section className= "min-h-screen ">
        <Hero/>
+       <Headline/>
+       <Features />
+      
       </section>
     </Layout>
   );
