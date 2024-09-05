8f89aa969c248ea42cb0138859119af3bc5ac568
Bettenoch <bettenoch254@gmail.com>
2024-09-05 18:43:30 +0300

    Change background color.

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -4,14 +4,14 @@ import { mainLinks } from "@/constants";
 const DesktopNav = () => {
   return (
     <header className="flex  w-full fixed z-50">
-      <nav className="flex gap-6 justify-between w-full mx-10 py-6 mt-2  bg-slate-300/60 rounded-full">
+      <nav className="flex gap-6 justify-between w-full mx-10 py-6 mt-2 bg-[#071330] rounded-full">
         <Link to="/" className="flex cursor-pointer items-center gap-1 pl-4">
-            <h1 className="text-3xl font-extrabold text-sky-800">FetchIt</h1>
+            <h1 className="text-3xl font-extrabold text-[#FDB515]"> PDFAi</h1>
         </Link>
         <ul className="flex items-center gap-4 pr-8">
           {mainLinks.map((link) => (
             <li key={link.label}>
-              <Link to={link.label} className=" flex text-lg font-bold text-stone-900">
+              <Link to={link.label} className=" flex text-lg font-bold text-stone-100">
                 {link.label}        
               </Link>
             </li>
