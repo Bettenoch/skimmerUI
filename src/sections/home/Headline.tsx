b3079f93da8f69571a5b161b7e44fea6445c64ef
Bettenoch <bettenoch254@gmail.com>
2024-09-05 18:41:32 +0300

    Create the headline page.

---
+++ src/sections/home/Headline.tsx
@@ -0,0 +1,15 @@
+import { Benefits } from "./Benefits"
+
+
+const Headline = () => {
+  return (
+    <section className=" flex flex-col w-full bg-[#C3CEDA] items-center justify-center">
+        <h1 className="text-4xl font-bold text-center pt-8 md:pt-12 lg:pt-32">Discover Our Powerful Features</h1>
+      <div className="pb-8 md:pb-12 lg:pb-32">
+        <Benefits/>
+      </div>
+    </section>
+  )
+}
+
+export default Headline
