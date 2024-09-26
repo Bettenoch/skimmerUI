5b8d2c11a44848316f6854e334825052b5542b3b
Bettenoch <bettenoch254@gmail.com>
2024-09-26 13:46:09 +0300

    Change dekstop nave style.

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 import { mainLinks } from "@/constants"; 
+import Cliptext from "./Cliptext";
 
 const DesktopNav = () => {
   return (
     <header className="flex  w-full fixed z-50">
-      <nav className="flex gap-6 justify-between w-full mx-10 py-6 mt-2 bg-[#071330] rounded-full">
+      <nav className="flex gap-6 justify-between w-full mx-10 py-6 mt-2 bg-light1 rounded-full">
         <Link to="/" className="flex cursor-pointer items-center gap-1 pl-4">
-            <h1 className="text-3xl font-extrabold text-[#FDB515]"> PDFAi</h1>
+          <Cliptext text="DocuChat" img="/background/goldbg2.png" />
         </Link>
         <ul className="flex items-center gap-4 pr-8">
           {mainLinks.map((link) => (
             <li key={link.label}>
-              <Link to={link.label} className=" flex text-lg font-bold text-stone-100">
+              <Link to={link.route} className=" flex text-lg font-bold text-stone-950">
                 {link.label}        
               </Link>
             </li>
